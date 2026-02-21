2077dfa679ea63cc9ecc722ead32ff1f3e38913e
hyoti33333 <gyfl1793@naver.com>
2026-02-22 07:05:09 +0900

    0222_16:00_update

--- LT/LineTrace.cpp
+++ LT/LineTrace.cpp
@@ -41,10 +41,10 @@ void driveLineFollow_detail(const IRSample& ir, uint8_t baseSpeed) { // &주소�
   }
   // 교차로 혹은 두꺼운 라인 : 111 
   else if (Lb && Cb && Rb) {
-    driveSetRaw(baseSpeed-40, baseSpeed-40); // 저속으로 일단 직진
+    driveSetRaw(baseSpeed-70, baseSpeed+70); // 강한 좌회전
   }
   // 검출 안됨 : 000
-  // 검출 안되는 상황은 main 안에서 처리됨
+  // 검출 안되는 상황은 main 안에서 처리됨 -> 이전 속도로 출력해야함
 }
 
 
